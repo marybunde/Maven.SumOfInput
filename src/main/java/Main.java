6454261e1bf91346e20288ec04730915fd9e96d2
marybunde <marybunde@gmail.com>
2022-07-04 22:39:34 -0400

    finished SumOfInput

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        //write a program prompting user to input number n
+        //program should respond by printing numbers 1-n
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input number: ");
+
+        int usrInput = input.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= usrInput; i++) {
+
+            sum += i;
+
+        }
+        System.out.println(sum);
 
     }
 }
